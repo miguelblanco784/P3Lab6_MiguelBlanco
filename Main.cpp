8a0aedf40022f7067785396df3839ea471c0d9c4
miguelblanco784 <miguelrobertoblanco@gmail.com>
2019-08-23 14:27:21 -0600

    Push Meno

--- Main.cpp
+++ Main.cpp
@@ -2,6 +2,40 @@
 using namespace std;
 
 int main(){
-	
-}
+	while(true){
+		int opcion;
+		cout<<"1- Administrar Ligas\n2- Administrar Equipos\n3- Administrar Jugadores\n4- Jugar Partido\n5- Cargar Archivo\n6- Guardar Archivo\n7- Ver tablas\n";
+		cin>>opcion;
+		switch(opcion){
+			case 1:{
+				
+				break;
+			}//fin case 1
+			case 2:{
+				
+				break;
+			}//fin case 2
+			case 3:{
+				
+				break;
+			}//fin case 3
+			case 4:{
+				
+				break;
+			}//fin case 4
+			case 5:{
+				
+				break;
+			}//fin case 5
+			case 6:{
+				
+				break;
+			}//fin case 6
+			case 7:{
+				
+				break;
+			}//fin case 7
+		}	
+	}//fin while infinito
+}//fin main
 
